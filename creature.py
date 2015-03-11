0ad57a63e21a051a6e7e3d6b77151f8c1cf8de2c
unknown <chris.nelson@SNY-L10013599.corp.ads.finisar.com>
2015-03-10 21:38:28 -0700

    Created hybrid creature of main and DoesCreatureWork branchess

--- creature.py
+++ creature.py
@@ -1,11 +1,14 @@
 from neuron import *
 from synapse import *
 from trueskill import Rating, quality_1vs1, rate_1vs1
+from NNetLibrary import *
 import numpy as np
 
 class Creature:
-    def __init__(self , neuronCount, inputCount, outputCount):
+    def __init__(self , neuronCount, inputCount, outputCount,maxCycles=50):
         self.neuronCount, self.inputCount, self.outputCount = neuronCount, inputCount, outputCount
+        self.maxCycles = maxCycles
+        self.cycles = 0
         self.neuronList, self.input, self.output, self.synapseList  = [], [], [], []
         self.fitness = 0.0
         self.expectedOutputs = []
@@ -39,21 +42,87 @@ class Creature:
 
     def run( self ): #no cycles or population, that info is internal to creature now
         runFitness = 0.0
-        for r in range( self.cycles ):
-
-            '''
-            for i in range ( self.inputCount ):
-                self.input[i].inbox = population.trainingCreature.input[i].inbox
-            '''
+        outputTracker = []
+        self.cycles = 0
+        for cyc in range( self.maxCycles):
             for n in self.neuronList:
                 n.run()
             for s in self.synapseList:
                 s.run()
+
+            self.cycles +=1
+
             totalCreatureOutputDifference = 0.0
             for Out in range(len(self.output)):
                 tOut = self.expectedOutputs[Out]
                 cOut = self.output[Out].outbox
                 totalCreatureOutputDifference += abs(tOut-cOut)
             runFitness = (runFitness + self.myGauss(0,1,totalCreatureOutputDifference) ) / 2
+
+            if cyc <= (self.neuronCount**2+10):#*(2.0/3.0):
+                outputTracker.append([])
+                for o in self.output:
+                    outputTracker[-1].append(o.outbox)
+            else:
+                newVal=[]
+                for o in self.output:
+                    newVal.append(o.outbox)
+                outputTracker = outputTracker[1:]+outputTracker[:1]
+                outputTracker[-1] = newVal
+
+                if checkConvergence(outputTracker):
+                    break
+
         self.fitness = (self.fitness + runFitness ) / 2
 
+
+
+def main():
+    neuronCount = 2
+    inputCount =1
+    outputCount = 1
+    MaxCycles = 45
+
+    inputSet = [0]
+    expOut = [0]
+
+    runs =6
+
+    demoCreature = Creature(neuronCount, inputCount, outputCount,MaxCycles)
+    for i in range(len(inputSet)):
+        demoCreature.input[i].inbox = [inputSet[i]]
+
+    demoCreature.expectedOutputs = expOut
+##    filename = r'C:\Users\chris.nelson\Desktop\NNet\CreatureDebugging\bestie4lyfe_2015_2_17_12_17_35'
+##    demoCreature = load_creature(filename)
+
+
+    print 'Creature Description:'
+    print '  Number of neurons:',len(demoCreature.neuronList)
+    print '  Number of synapses:',len(demoCreature.synapseList)
+    print '  Number of inputs:',len(demoCreature.input)
+    print '  Number of outputs:',len(demoCreature.output)
+    print '  Max cycles to run:',demoCreature.cycles
+    print '  Initial ELO: ',demoCreature.ELO
+    print '  Inputs: ',inputSet
+    print '  Expected Outputs: ',demoCreature.expectedOutputs
+
+    demoCreature.run()
+
+    outputs = []
+    for outp in demoCreature.output:
+        outputs.append(outp.outbox)
+    print '  Outputs: ',outputs
+    print '  Cycles ran: ',demoCreature.cycles
+
+
+    print ''
+    print 'Repeatability test:'
+    inputSets = [[0],[1]]
+    testCreatureRepeatability(demoCreature,inputSets,runs)
+
+
+    #seeCreature(demoCreature)
+
+if __name__ == '__main__':
+    main()
--- neuron.py
+++ neuron.py
@@ -4,9 +4,10 @@ class Neuron:
         self.inbox = [gauss(0,1)]
         self.value, self.threshold, self.outbox, self.prevOutbox = gauss(0,1), gauss(0,1), gauss(0,1), gauss(0,1)
         self.isInput, self.isOutput = 0, 0
-
+        self.maxVal = 10000
+    '''
     def run(self):
-        '''
+
         self.prevOutbox = self.outbox
         if self.isOutput == 0:#If not an output neuron
             self.outbox = 0.0
@@ -16,5 +17,15 @@ class Neuron:
             self.value = 0.0
         if self.isInput == 0:#If not an input neuron
             self.inbox = [0.0]
-        '''
-        self.outbox += sum(self.inbox) / (float(len(self.inbox)))
+
+        #self.outbox += sum(self.inbox) / (float(len(self.inbox)))
+
+    '''
+    def run(self):
+        self.prevOutbox = self.outbox
+        avgInput = sum(self.inbox)/float(len(self.inbox)+1)
+        self.value += min(self.maxVal,max(avgInput,-1*self.maxVal))
+        if (self.value >= self.threshold):
+            self.outbox = self.value
+            self.value = 0.0
+        self.inbox = []
